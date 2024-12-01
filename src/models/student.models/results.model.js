349cddc3ec3efe9f76d1d5d4c18a798f7c43508f
Sumit Kumar <sumitkumar84390@gmail.com>
2024-12-01 15:36:48 +0530

    Update result model

--- src/models/student.models/results.model.js
+++ src/models/student.models/results.model.js
@@ -1,52 +1,61 @@
 import mongoose, { Schema } from "mongoose";
 
 const resultSchema = new Schema({
-  studentID: {
-    type: Schema.Types.ObjectId,
-    ref: "Student",
-    required: true,
-  },
-  session: {
-    type: String,
-    required: true,
-  },
-  totalMarks: {
-    type: Number,
-    required: true,
-  },
-  obtainMarks: {
-    type: Number,
-  },
-  resultType: {
-    type: String,
-    required: true,
-  },
-  resultStatus: {
-    type: String,
-  },
-  subjects: [
-    {
-      subjectID: {
+    studentID: {
         type: Schema.Types.ObjectId,
-        ref: "Subject",
+        ref: "Student",
         required: true,
-      },
-      obtainMarks: {
-        type: Number,
-        required: true,
-        default: -1,
-      },
-      internalMarks: {
-        type: Number,
-        required: true,
-        default: -1,
-      },
     },
-  ],
-  cop: {
-    type: [Schema.Types.ObjectId],
-    ref: "Subject",
-  },
+    session: {
+        type: String,
+        required: true,
+    },
+    totalMarks: {
+        type: Number,
+        required: true,
+    },
+    obtainMarks: {
+        type: Number,
+    },
+    resultType: {
+        type: String,
+        required: true,
+    },
+    resultStatus: {
+        type: String,
+    },
+    passingMarks: {
+        type: Number,
+        required: true,
+        default: 30,
+    },
+    subjects: [
+        {
+            subjectID: {
+                type: Schema.Types.ObjectId,
+                ref: "Subject",
+                required: true,
+            },
+            obtainMarks: {
+                type: Number,
+                required: true,
+                default: -1,
+            },
+            internalMarks: {
+                type: Number,
+                required: true,
+                default: -1,
+            },
+            grade: {
+                type: String,
+                required: true,
+            },
+        },
+    ],
+    cop: {
+        type: [Schema.Types.ObjectId],
+        ref: "Subject",
+    },
 });
 
 export const Result = mongoose.model("Result", resultSchema);
